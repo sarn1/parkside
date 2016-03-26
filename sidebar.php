4d50f86195556d0d6174ee78e0ea45e5d7a3d7de
Webvolution <spham@webvolutionchicago.com>
2016-03-25 20:10:37 -0500

    testing & fix responsive menu

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,1 @@
-<div class="side_widget u-pull-right">
-<div class="head primary">
-  <h2>TESTIMONIALS</h2>
-</div>
-<div class="body">
-  <?php echo do_shortcode('[content_block id=89]'); ?>
-</div>
-</div>
+<div class="side_widget u-pull-right"><div class="head primary"><h2>TESTIMONIALS</h2></div><div class="body"> <?php echo do_shortcode('[content_block id=89]'); ?></div></div>
